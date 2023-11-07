9d7e11441295eca9e0fcdaf5a0b21eed91bd5fb0
Vasco Lopes <vascocajada@gmail.com>
2023-11-07 20:44:39 +0000

    hot reload and dev server

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp } from 'vue/dist/vue.esm-bundler';
+
+import ExampleCounter from "./components/ExampleCounter.vue";
+
+const app = createApp({});
+
+app.component("example-counter", ExampleCounter);
+
+const mountedApp = app.mount("#app");
