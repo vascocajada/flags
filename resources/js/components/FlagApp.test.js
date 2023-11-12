9802b30bd82adbde2e1c69a4bf59de6a7f65c5df
Vasco Lopes <vascocajada@gmail.com>
2023-11-12 18:24:32 +0000

    fix frontend tests

--- resources/js/components/FlagApp.test.js
+++ resources/js/components/FlagApp.test.js
@@ -3,11 +3,18 @@ import FlagApp from './FlagApp.vue'
 import axios from 'axios'
 import mockFlagListJson from '../mocks/mockFlagList.json'
 
+jest.mock('@auth0/auth0-vue', () => ({
+  useAuth0: () => ({
+    getAccessTokenSilently: () => 'mockToken'
+  })
+}))
+
+
 test('Main page displays title', () => {
   const wrapper = shallowMount(FlagApp, {})
 
   // Assert the rendered text of the component
-  expect(wrapper.text()).toContain('Flags App')
+  expect(wrapper.text()).toContain('flags app')
 })
 
 test('Main page displays spinner', () => {
@@ -21,7 +28,7 @@ test('Main page displays flags', async () => {
 
   jest.spyOn(axios, 'get').mockResolvedValue(mockFlagList)
 
-  const wrapper = mount(FlagApp, {})
+  const wrapper = await mount(FlagApp, {})
 
   expect(axios.get).toHaveBeenCalledTimes(1)
   expect(axios.get).toHaveBeenCalledWith('/api/flag-list')
